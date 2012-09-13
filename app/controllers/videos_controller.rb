adb947ec3e6dcb4ffee68a77220fbab0b876ea9a
Nick Drost <ndrost@salesforce.com>
2012-09-13 18:49:06 -0500

    Initial Push

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -19,4 +19,20 @@ class VideosController < ApplicationController
     @videos=Panda::Video.all
     @encodings = Panda::Encoding.all
   end
+
+  def edit
+    @podcast = Video.find(params[:id])
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.panda_video.delete
+    @video.destroy
+
+    respond_to do |format|
+      format.html { redirect_to :action => :index }
+      format.xml  { head :ok }
+    end
+  end
+
 end
